82d3d54bb94e213bbc80bbe66fd3062f4a75d733
hackhawk-arch <macharialucas679@gmail.com>
2026-01-27 15:06:51 +0300

    Exercise 1.5: Todo app responds to GET /

--- To-do/1.2/index.js
+++ To-do/1.2/index.js
@@ -4,10 +4,24 @@
 const http = require("http");
 
 const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.MESSAGE || "Todo App is running";
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Todo app server");
+    if (req.url == "/" && req.method == "GET") {
+        res.writeHead(200, { "Content-Type": "text/html"});
+        res.end(`
+            <html>
+                <head><title>Todo App</title></head>
+                <body>
+                    <h1>${MESSAGE}</h1>
+                    <p>Server is running on port ${PORT}</p>
+                </body>
+            </html>
+        `);
+    } else {
+        res.writeHead(404);
+        res.end("Not Found");
+    }
 });
 
 server.listen(PORT, () => {
