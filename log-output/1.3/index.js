4756d4425d47bd3334f7535c1d6da882bff748fb
hackhawk-arch <macharialucas679@gmail.com>
2026-01-29 13:28:48 +0300

    Exercise 1.7: Add status endpoint and Ingress to log output

--- log-output/1.3/index.js
+++ log-output/1.3/index.js
@@ -1,5 +1,7 @@
+const http = require("http");
 const crypto = require("crypto");
 
+const PORT = process.env.PORT || 3000;
 const randomString = crypto.randomBytes(16).toString("hex");
 
 console.log("Application started");
@@ -8,3 +10,22 @@ setInterval(() => {
     const timestamp = new Date().toISOString();
     console.log(`${timestamp}: ${randomString}`);
 }, 5000);
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/" && req.method === "GET") {
+        const timestamp = new Date().toISOString();
+        res.end(
+            JSON.stringify({
+                timestamp,
+                randomString,
+            })
+        );
+    } else {
+        res.writeHeadf(404);
+        res.end("Not Found");
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`HTTP server running on port ${PORT}`);
+});
